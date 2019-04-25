feeb4eda2d766af65683ab8dc74750157851b06c
Андрей Чепрасов <ae.cheprasov@gmail.com>
2019-04-25 18:54:34 +0300

    обновлен скрипт сборки ffmpeg

--- externals/3rdparty/ffmpeg/build.sh
+++ externals/3rdparty/ffmpeg/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sudo apt-get -y install autoconf automake libtool build-essential cmake git subversion mercurial pkg-config
+#sudo apt-get -y install autoconf automake libtool build-essential cmake git pkg-config
 
 missing_deps=""
 
@@ -16,8 +16,6 @@ test_cmd libtoolize libtool
 test_cmd make build-essential
 test_cmd cmake cmake
 test_cmd git git
-test_cmd svn subversion
-test_cmd hg mercurial
 test_cmd pkg-config pkg-config
 
 if [[ ! -z "$missing_deps" ]]; then
@@ -39,7 +37,7 @@ PATH_ORIG=$PATH
 PATH=$PATH_ROOT/lib:$PATH_ROOT/include:$PATH_ROOT/bin:$PATH_ORIG
 
 
-cpu_count=`grep -c '^processor' /proc/cpuinfo`
+cpu_count=`nproc`
 
 
 export PKG_CONFIG_PATH="$PATH_BASE/lib/pkgconfig"
@@ -51,39 +49,12 @@ if [ ! -e $PATH_BUILD ]; then
 fi
 
 
-build_yasm() {
-  cd $PATH_BUILD
-
-  if [ ! -e yasm ]; then
-    git clone git://github.com/yasm/yasm.git
-    cd yasm
-
-  else
-    cd yasm
-    git reset --hard
-    git clean -dfx
-    git pull
-  fi
-
-  autoreconf -fiv
-  CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin"
-  make -j$cpu_count
-  make install
-}
-
 build_nasm() {
   cd $PATH_BUILD
 
   if [ ! -e nasm ]; then
-    git clone git://repo.or.cz/nasm.git
+    git clone git://repo.or.cz/nasm.git --branch nasm-2.14.02 --single-branch --depth 1
     cd nasm
-    # git checkout nasm-2.13.03
-
-  else
-    cd nasm
-    git reset --hard
-    git clean -dfx
-    git pull
   fi
 
   autoreconf -fiv
@@ -92,32 +63,11 @@ build_nasm() {
   make install
 }
 
-build_numa() {
-  cd $PATH_BUILD
-
-  if [ ! -e numactl ]; then
-    git clone https://github.com/numactl/numactl
-    cd numactl
-
-  else
-    cd numactl
-    git reset --hard
-    git clean -dfx
-    git pull
-  fi
-
-  autoreconf -fiv
-  CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin"
-  make -j$cpu_count
-  make install
-}
-
-
 build_opus() {
   cd $PATH_BUILD
 
   if [ ! -e opus ]; then
-    git clone https://github.com/xiph/opus.git
+    git clone --depth 1 https://github.com/xiph/opus.git
     cd opus
 
   else
@@ -137,7 +87,7 @@ build_ogg() {
   cd $PATH_BUILD
 
   if [ ! -e ogg ]; then
-    git clone https://github.com/xiph/ogg.git
+    git clone --depth 1 https://github.com/xiph/ogg.git
     cd ogg
 
   else
@@ -157,7 +107,7 @@ build_vorbis() {
   cd $PATH_BUILD
 
   if [ ! -e vorbis ]; then
-    git clone https://github.com/xiph/vorbis.git
+    git clone --depth 1 https://github.com/xiph/vorbis.git
     cd vorbis
 
   else
@@ -173,29 +123,11 @@ build_vorbis() {
   make install
 }
 
-
-build_lame() {
-  cd $PATH_BUILD
-
-  if [ ! -e lame ]; then
-    svn checkout https://svn.code.sf.net/p/lame/svn/trunk/lame lame
-    cd lame
-
-  else
-    cd lame
-    svn update
-  fi
-
-  CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin" --disable-shared --enable-nasm CFLAGS="$str_opt"
-  make -j$cpu_count
-  make install
-}
-
 build_aac() {
   cd $PATH_BUILD
 
   if [ ! -e fdk-aac ]; then
-    git clone git://git.code.sf.net/p/opencore-amr/fdk-aac
+    git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
     cd fdk-aac
 
   else
@@ -211,12 +143,11 @@ build_aac() {
   make install
 }
 
-
 build_ff() {
   cd $PATH_BUILD
 
   if [ ! -e ffmpeg ]; then
-    git clone git://source.ffmpeg.org/ffmpeg
+    git clone --depth 1 git://source.ffmpeg.org/ffmpeg
     cd ffmpeg
 
   else
@@ -286,6 +217,5 @@ build_nasm
 build_opus
 build_ogg
 build_vorbis
-#build_lame
 build_aac
 build_ff
